1571d9daa2818b2a584ede0efff10696c5919d38
raycbn <74503112+raycbn@users.noreply.github.com>
2026-06-21 00:27:05 +0200

    Update stats.js

--- js/stats.js
+++ js/stats.js
@@ -24,29 +24,13 @@ function saveStats(stats) {
     );
 }
 
-function resetStats() {
-
-    localStorage.removeItem(STORAGE_KEY);
-
-    updateStatsUI({
-        correct: 0,
-        wrong: 0
-    });
-}
-
 function updateStatsUI(stats) {
-
-    const correct =
-        stats.correct || 0;
-
-    const wrong =
-        stats.wrong || 0;
 
     document.getElementById(
         'correctCount'
-    ).innerText = correct;
+    ).innerText = stats.correct;
 
     document.getElementById(
         'wrongCount'
-    ).innerText = wrong;
+    ).innerText = stats.wrong;
 }
