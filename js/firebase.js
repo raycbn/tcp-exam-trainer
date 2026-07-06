e3d37a591815a2dad394e48d3166ffeb2c40bf3f
raycbn <74503112+raycbn@users.noreply.github.com>
2026-07-06 14:16:42 +0200

    Add Firebase configuration and initialization

---
+++ js/firebase.js
@@ -0,0 +1,44 @@
+// =========================
+// FIREBASE CONFIG
+// =========================
+
+import { initializeApp } 
+from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+
+import { getAuth } 
+from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
+
+
+// =========================
+// CONFIGURACIÓN FIREBASE
+// =========================
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyDZ0FULdloHS1OKe0X7SM4THnQmQF1ztBQ",
+
+    authDomain: "eliteaircrewtrainer.firebaseapp.com",
+
+    projectId: "eliteaircrewtrainer",
+
+    storageBucket: "eliteaircrewtrainer.firebasestorage.app",
+
+    messagingSenderId: "130225639745",
+
+    appId: "1:130225639745:web:bd0fa6e1b195bc9a2f11c0"
+
+};
+
+
+// =========================
+// INIT
+// =========================
+
+const app = initializeApp(firebaseConfig);
+
+
+// =========================
+// EXPORTS
+// =========================
+
+export const auth = getAuth(app);
