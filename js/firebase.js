a01276ecf6ed6c052401c5e30dc8ee73a14209ed
raycbn <74503112+raycbn@users.noreply.github.com>
2026-07-07 00:23:23 +0200

    Refactor Firebase imports and configuration formatting

--- js/firebase.js
+++ js/firebase.js
@@ -2,43 +2,87 @@
 // FIREBASE CONFIG
 // =========================
 
-import { initializeApp } 
+
+import { initializeApp }
+
 from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
 
-import { getAuth } 
+
+
+import { getAuth }
+
 from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
+
+
+
+import { getFirestore }
+
+from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+
+
+
 
 
 // =========================
 // CONFIGURACIÓN FIREBASE
 // =========================
 
+
 const firebaseConfig = {
 
-    apiKey: "AIzaSyDZ0FULdloHS1OKe0X7SM4THnQmQF1ztBQ",
 
-    authDomain: "eliteaircrewtrainer.firebaseapp.com",
+    apiKey:
+    "AIzaSyDZ0FULdloHS1OKe0X7SM4THnQmQF1ztBQ",
 
-    projectId: "eliteaircrewtrainer",
 
-    storageBucket: "eliteaircrewtrainer.firebasestorage.app",
+    authDomain:
+    "eliteaircrewtrainer.firebaseapp.com",
 
-    messagingSenderId: "130225639745",
 
-    appId: "1:130225639745:web:bd0fa6e1b195bc9a2f11c0"
+    projectId:
+    "eliteaircrewtrainer",
+
+
+    storageBucket:
+    "eliteaircrewtrainer.firebasestorage.app",
+
+
+    messagingSenderId:
+    "130225639745",
+
+
+    appId:
+    "1:130225639745:web:bd0fa6e1b195bc9a2f11c0"
+
 
 };
+
+
 
 
 // =========================
 // INIT
 // =========================
 
-const app = initializeApp(firebaseConfig);
+
+const app =
+
+initializeApp(firebaseConfig);
+
+
 
 
 // =========================
 // EXPORTS
 // =========================
 
-export const auth = getAuth(app);
+
+export const auth =
+
+getAuth(app);
+
+
+
+export const db =
+
+getFirestore(app);
