4a1c81bc6a5aa54c7034272d269dccd86babf8ab
raycbn <74503112+raycbn@users.noreply.github.com>
2026-07-06 15:13:53 +0200

    Remove user registration logic from auth.js
    Removed registration functionality and related code.

--- js/auth.js
+++ js/auth.js
@@ -1,9 +1,7 @@
 import { auth } from "./firebase.js";
 
-
 import {
 
-    createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
     sendPasswordResetEmail
 
@@ -17,126 +15,66 @@ import {
 const email =
 document.getElementById("email");
 
-
 const password =
 document.getElementById("password");
-
 
 const loginBtn =
 document.getElementById("loginBtn");
 
-
-const registerBtn =
-document.getElementById("registerBtn");
-
-
 const resetBtn =
 document.getElementById("resetBtn");
 
-
 const message =
 document.getElementById("authMessage");
-
 
 
 // =========================
 // LOGIN
 // =========================
 
-
 loginBtn.onclick = async () => {
 
     try {
 
         await signInWithEmailAndPassword(
-
             auth,
             email.value,
             password.value
-
         );
-
 
         window.location.href =
         "index.html";
 
-
-    } catch(error) {
-
+    } catch (error) {
 
         message.innerText =
         "❌ Usuario o contraseña incorrectos";
 
-
     }
 
 };
-
-
-
-// =========================
-// REGISTRO
-// =========================
-
-
-registerBtn.onclick = async () => {
-
-    try {
-
-        await createUserWithEmailAndPassword(
-
-            auth,
-            email.value,
-            password.value
-
-        );
-
-
-        window.location.href =
-        "index.html";
-
-
-    } catch(error) {
-
-
-        message.innerText =
-        "❌ No se pudo crear la cuenta";
-
-
-    }
-
-};
-
 
 
 // =========================
 // RECUPERAR PASSWORD
 // =========================
 
-
 resetBtn.onclick = async () => {
 
     try {
 
-
         await sendPasswordResetEmail(
-
             auth,
             email.value
-
         );
-
 
         message.innerText =
         "📩 Email de recuperación enviado";
 
-
-    } catch(error) {
-
+    } catch (error) {
 
         message.innerText =
         "Introduce tu correo primero";
-
 
     }
 
