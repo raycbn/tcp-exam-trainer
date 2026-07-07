1a3befe6dd3cfc1c6177b6ee693527497c0471cb
raycbn <74503112+raycbn@users.noreply.github.com>
2026-07-07 12:30:51 +0200

    Update admin-guard.js

--- js/admin-guard.js
+++ js/admin-guard.js
@@ -1,52 +1,16 @@
 import { auth } from "./firebase.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
 
+const ADMIN_EMAIL = "rayvf2002@gmail.com";
 
-import {
-
-onAuthStateChanged
-
-} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
-
-
-
-const ADMIN_EMAIL =
-
-"rayvf2002@gmail.com";
-
-
-
-onAuthStateChanged(auth,user=>{
-
-
-    if(!user){
-
-        window.location.href =
-        "login.html";
-
+onAuthStateChanged(auth, user => {
+    if (!user) {
+        window.location.replace("login.html");
         return;
-
     }
 
-
-
-    if(
-
-        user.email !== ADMIN_EMAIL
-
-    ){
-
-
-        alert(
-        "Acceso no autorizado"
-        );
-
-
-        window.location.href =
-        "index.html";
-
-
+    if (user.email !== ADMIN_EMAIL) {
+        alert("Acceso no autorizado");
+        window.location.replace("index.html");
     }
-
-
-
 });
